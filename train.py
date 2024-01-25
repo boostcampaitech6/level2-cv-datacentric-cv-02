9e78a09e676c41a9b4edf7510337b24dece0caa5
root <solee328@gmail.com>
2024-01-25 05:03:29 +0000

    feat: validation with wandb

--- train.py
+++ train.py
@@ -15,8 +15,14 @@ from tqdm import tqdm
 from east_dataset import EASTDataset
 from dataset import SceneTextDataset
 from model import EAST
+from detect import get_bboxes
+from deteval import calc_deteval_metrics
 
 import wandb
+import glob
+import re
+import numpy as np
+from pathlib import Path
 
 
 def parse_args():
@@ -59,8 +65,32 @@ def seed_everything(seed):
     random.seed(seed)
 
 
+def increment_path(path, exist_ok=False):
+    """Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+
+    Args:
+        path (str or pathlib.Path): f"{model_dir}/{args.name}".
+        exist_ok (bool): whether increment path (increment if False).
+    """
+    path = Path(path)
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}*")
+        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]
+        n = max(i) + 1 if i else 2
+        return f"{path}{n}"
+
+
 def do_training(data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
                 learning_rate, max_epoch, save_interval, ignore_tags, seed, run_name):
+
+    ckpt_fpath = increment_path(os.path.join(model_dir, run_name))
+    if not osp.exists(ckpt_fpath):
+                os.makedirs(ckpt_fpath)
+                
+    max_hmean = 0.0
 
     wandb.init(
         project='ocr',
@@ -68,19 +98,34 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
         name=run_name,
     )
     
-    dataset = SceneTextDataset(
+    dataset_train = SceneTextDataset(
         data_dir,
-        split='train',
+        split='0_train',
         image_size=image_size,
         crop_size=input_size,
         ignore_tags=ignore_tags
     )
-    dataset = EASTDataset(dataset)
-    num_batches = math.ceil(len(dataset) / batch_size)
+    dataset_valid = SceneTextDataset(
+        data_dir,
+        split='0_valid',
+        image_size=image_size,
+        crop_size=input_size,
+        ignore_tags=ignore_tags
+    )
+    dataset_train = EASTDataset(dataset_train)
+    dataset_valid = EASTDataset(dataset_valid)
+
+    num_batches = math.ceil(len(dataset_train) / batch_size)
     train_loader = DataLoader(
-        dataset,
+        dataset_train,
         batch_size=batch_size,
         shuffle=True,
+        num_workers=num_workers
+    )
+    valid_loader = DataLoader(
+        dataset_valid,
+        batch_size=1,
+        shuffle=False,
         num_workers=num_workers
     )
 
@@ -89,7 +134,6 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
-
     
     model.train()
     for epoch in range(max_epoch):
@@ -125,12 +169,54 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
         print('Mean loss: {:.4f} | Elapsed time: {}'.format(
             epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
 
-        if (epoch + 1) % save_interval == 0:
-            if not osp.exists(model_dir):
-                os.makedirs(model_dir)
+        # valid
+        print('[Epoch {} validation...]'.format(epoch + 1))
+        valid_cls, valid_angle, valid_iou = 0.0, 0.0, 0.0
+        valid_precision, valid_recall, valid_hmean = 0.0, 0.0, 0.0
 
-            ckpt_fpath = osp.join(model_dir, 'latest.pth')
-            torch.save(model.state_dict(), ckpt_fpath)
+        model.eval()
+        with torch.no_grad():
+            for img, gt_score_map, gt_geo_map, roi_mask in valid_loader:
+                # loss
+                _, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+                valid_cls += extra_info['cls_loss']
+                valid_angle += extra_info['angle_loss']
+                valid_iou += extra_info['iou_loss'] 
+
+                # deteval
+                pred_score, pred_geo = model(img.to(device))
+
+                pred_bboxes = get_bboxes(pred_score.cpu().numpy()[0], pred_geo.cpu().numpy()[0])
+                gt_bboxes = get_bboxes(gt_score_map.numpy()[0], gt_geo_map.numpy()[0])
+
+                pred_bboxes = {0: np.expand_dims(pred_bboxes, axis=0)}
+                gt_bboxes = {0: np.expand_dims(gt_bboxes, axis=0)}
+
+                metric = calc_deteval_metrics(pred_bboxes, gt_bboxes, transcriptions_dict={0: [""]*len(gt_bboxes[0][0])})
+                valid_precision += metric['total']['precision']
+                valid_recall += metric['total']['recall']
+                valid_hmean += metric['total']['hmean']
+
+            wandb.log({
+                'valid_cls': valid_cls/len(dataset_valid),
+                'valid_angle': valid_angle/len(dataset_valid),
+                'valid_iou': valid_iou/len(dataset_valid),
+                'valid_precision': valid_precision/len(dataset_valid),
+                'valid_recall': valid_recall/len(dataset_valid),
+                'valid_hmean': valid_hmean/len(dataset_valid),
+            })
+        model.train()
+
+        # model save(last)
+        path = osp.join(ckpt_fpath, 'latest.pth')
+        torch.save(model.state_dict(), path)
+        
+        # model save(best)
+        current_hmean = valid_hmean/len(dataset_valid)
+        if current_hmean > max_hmean:
+            max_hmean = current_hmean
+            path = osp.join(ckpt_fpath, 'best_hmean.pth')
+            torch.save(model.state_dict(), path)
 
         # wandb logging(epoch)
         wandb.log({
